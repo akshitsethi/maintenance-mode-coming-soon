e204c7f1fb42316978c1a1b98401ed2caa099526
Akshit Sethi <akshitsethi@gmail.com>
2020-03-28 23:26:17 +0530

    Added the uninstall file

--- src/uninstall.php
+++ src/uninstall.php
@@ -3,10 +3,16 @@
  * File which gets called on plugin uninstall. Since the plugin does not do any
  * sort of setup, nothing is done over here.
  *
- * @link    https://akshitsethi.com
  * @since 	1.0.0
  * @package AkshitSethi/Plugins/MaintenanceMode
- *
- * Checking whether the file is called by the Wordpress uninstall action. If not,
- * then exit and prevent unauthorized access.
  */
+
+namespace AkshitSethi\Plugins\MaintenanceMode;
+
+// Prevent unauthorized access
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+  exit;
+}
+
+// Remove plugin options from the database
+delete_option( Config::DB_OPTION );
