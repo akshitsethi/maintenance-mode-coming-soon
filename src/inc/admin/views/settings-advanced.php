57e856d0c31e0652f4170765d8f36862eaf15613
Akshit Sethi <akshitsethi@gmail.com>
2020-03-29 15:46:29 +0530

    Further refactoring of backend options

--- src/inc/admin/views/settings-advanced.php
+++ src/inc/admin/views/settings-advanced.php
@@ -16,26 +16,26 @@ use AkshitSethi\Plugins\MaintenanceMode\Config;
 
 		<div class="as-section-content">
 			<div class="as-form-group">
-				<label for="signals_csmm_disable" class="as-strong"><?php esc_html_e( 'Use Custom HTML only', 'classic-coming-soon-maintenance-mode' ); ?></label>
-				<input type="checkbox" class="as-form-ios" name="signals_csmm_disable" value="1"<?php checked( '1', $options['disable_settings'] ); ?>>
+				<label for="<?php echo Config::PREFIX . 'disable'; ?>" class="as-strong"><?php esc_html_e( 'Use Custom HTML only', 'classic-coming-soon-maintenance-mode' ); ?></label>
+				<input type="checkbox" class="as-form-ios" name="<?php echo Config::PREFIX . 'disable'; ?>" value="1"<?php checked( true, $options['disable_settings'] ); ?>>
 
 				<p class="as-form-help-block"><?php esc_html_e( 'If you enable this option, the plugin will ignore everything except the HTML you provide.', 'classic-coming-soon-maintenance-mode' ); ?></p>
 				<p class="as-form-help-block"><?php esc_html_e( 'Basically, you will have a blank template which you can fill with your provided html content. Only basic css gets added by the plugin which does the task of browser styling reset. You should style your html content either inline or by inserting styling in the custom css section. In short, use this option only if you know what you are doing.', 'classic-coming-soon-maintenance-mode' ); ?></p>
 			</div>
 
 			<div class="as-form-group">
-				<label for="signals_csmm_html" class="as-strong"><?php esc_html_e( 'Custom HTML', 'classic-coming-soon-maintenance-mode' ); ?></label>
-				<div id="signals_csmm_html_editor"></div>
-				<textarea name="signals_csmm_html" id="signals_csmm_html" rows="8" placeholder="<?php esc_html_e( 'Custom HTML for the plugin', 'classic-coming-soon-maintenance-mode' ); ?>"><?php echo stripslashes( $options['custom_html'] ); ?></textarea>
+				<label for="<?php echo Config::PREFIX . 'html'; ?>" class="as-strong"><?php esc_html_e( 'Custom HTML', 'classic-coming-soon-maintenance-mode' ); ?></label>
+				<div id="<?php echo Config::PREFIX . 'html_editor'; ?>"></div>
+				<textarea name="<?php echo Config::PREFIX . 'html'; ?>" id="<?php echo Config::PREFIX . 'html'; ?>" rows="8" placeholder="<?php esc_html_e( 'Custom HTML for the plugin', 'classic-coming-soon-maintenance-mode' ); ?>"><?php echo stripslashes( $options['custom_html'] ); ?></textarea>
 
-				<p class="as-form-help-block"><?php echo __( 'Custom HTML for the plugin goes over here. Please note that ', 'classic-coming-soon-maintenance-mode' ) . '<i style="color: #f96773">' . __( '[html], [head], [title], [meta], [body], and similar tags', 'classic-coming-soon-maintenance-mode' ) . '</i>' . __( ' are not required. Only provide content HTML for the page.', 'classic-coming-soon-maintenance-mode' ); ?></p>
+				<p class="as-form-help-block"><?php echo esc_html__( 'Custom HTML for the plugin goes over here. Please note that ', 'classic-coming-soon-maintenance-mode' ) . '<i style="color: #f96773">' . esc_html__( '[html], [head], [title], [meta], [body], and similar tags', 'classic-coming-soon-maintenance-mode' ) . '</i>' . __( ' are not required. Only provide content HTML for the page.', 'classic-coming-soon-maintenance-mode' ); ?></p>
 				<p class="as-form-help-block"><?php esc_html_e( 'To insert subscription form anywhere in the html, simply use the placeholder <strong>{{form}}</strong> and you are done.', 'classic-coming-soon-maintenance-mode' ); ?></p>
 			</div>
 
 			<div class="as-form-group">
-				<label for="signals_csmm_css" class="as-strong"><?php esc_html_e( 'Custom CSS', 'classic-coming-soon-maintenance-mode' ); ?></label>
-				<div id="signals_csmm_css_editor"></div>
-				<textarea name="signals_csmm_css" id="signals_csmm_css" class="Signals_csmm_Block" rows="8" placeholder="<?php esc_html_e( 'Custom CSS for the plugin', 'classic-coming-soon-maintenance-mode' ); ?>"><?php echo stripslashes( $options['custom_css'] ); ?></textarea>
+				<label for="<?php echo Config::PREFIX . 'css'; ?>" class="as-strong"><?php esc_html_e( 'Custom CSS', 'classic-coming-soon-maintenance-mode' ); ?></label>
+				<div id="<?php echo Config::PREFIX . 'css_editor'; ?>"></div>
+				<textarea name="<?php echo Config::PREFIX . 'css'; ?>" id="<?php echo Config::PREFIX . 'css'; ?>" class="Signals_csmm_Block" rows="8" placeholder="<?php esc_html_e( 'Custom CSS for the plugin', 'classic-coming-soon-maintenance-mode' ); ?>"><?php echo stripslashes( $options['custom_css'] ); ?></textarea>
 
 				<p class="as-form-help-block"><?php esc_html_e( 'Custom CSS for the plugin goes over here.', 'classic-coming-soon-maintenance-mode' ); ?></p>
 			</div>
--- src/inc/admin/views/settings-form.php
+++ src/inc/admin/views/settings-form.php
@@ -17,37 +17,37 @@ use AkshitSethi\Plugins\MaintenanceMode\Config;
 		<div class="as-section-content">
 			<div class="as-double-group as-clearfix">
 				<div class="as-form-group">
-					<label for="signals_csmm_input_text" class="as-strong"><?php esc_html_e( 'Input Text', 'classic-coming-soon-maintenance-mode' ); ?></label>
-					<input type="text" name="signals_csmm_input_text" id="signals_csmm_input_text" value="<?php esc_attr_e( stripslashes( $options['input_text'] ) ); ?>" placeholder="<?php esc_html_e( 'Text for the Input field', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control">
+					<label for="<?php echo Config::PREFIX . 'input_text'; ?>" class="as-strong"><?php esc_html_e( 'Input Text', 'classic-coming-soon-maintenance-mode' ); ?></label>
+					<input type="text" name="<?php echo Config::PREFIX . 'input_text'; ?>" id="<?php echo Config::PREFIX . 'input_text'; ?>" value="<?php esc_attr_e( stripslashes( $options['input_text'] ) ); ?>" placeholder="<?php esc_html_e( 'Text for the Input field', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control">
 
 					<p class="as-form-help-block"><?php esc_html_e( 'Enter the text which you would like to use as a placeholder text for the text input field.', 'classic-coming-soon-maintenance-mode' ); ?></p>
 				</div>
 
 				<div class="as-form-group">
-					<label for="signals_csmm_button_text" class="as-strong"><?php esc_html_e( 'Button Text', 'classic-coming-soon-maintenance-mode' ); ?></label>
-					<input type="text" name="signals_csmm_button_text" id="signals_csmm_button_text" value="<?php esc_attr_e( stripslashes( $options['button_text'] ) ); ?>" placeholder="<?php esc_html_e( 'Text for the Button', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control">
+					<label for="<?php echo Config::PREFIX . 'button_text'; ?>" class="as-strong"><?php esc_html_e( 'Button Text', 'classic-coming-soon-maintenance-mode' ); ?></label>
+					<input type="text" name="<?php echo Config::PREFIX . 'button_text'; ?>" id="<?php echo Config::PREFIX . 'button_text'; ?>" value="<?php esc_attr_e( stripslashes( $options['button_text'] ) ); ?>" placeholder="<?php esc_html_e( 'Text for the Button', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control">
 
 					<p class="as-form-help-block"><?php esc_html_e( 'Enter the text for the button. Usually, it will be "Subscribe" or something like that.', 'classic-coming-soon-maintenance-mode' ); ?></p>
 				</div>
 			</div>
 
 			<div class="as-form-group">
-				<label for="signals_csmm_ignore_styles" class="as-strong"><?php esc_html_e( 'Ignore Default Form Styles?', 'classic-coming-soon-maintenance-mode' ); ?></label>
-				<input type="checkbox" class="as-form-ios" name="signals_csmm_ignore_styles" value="1"<?php checked( '1', $options['ignore_form_styles'] ); ?>>
+				<label for="<?php echo Config::PREFIX . 'ignore_styles'; ?>" class="as-strong"><?php esc_html_e( 'Ignore Default Form Styles?', 'classic-coming-soon-maintenance-mode' ); ?></label>
+				<input type="checkbox" class="as-form-ios" name="<?php echo Config::PREFIX . 'ignore_styles'; ?>" value="1"<?php checked( true, $options['ignore_form_styles'] ); ?>>
 
 				<p class="as-form-help-block"><?php esc_html_e( 'Enable this option if you would like to use your custom form styles. The settings below will only be applicable when this option is turned on.', 'classic-coming-soon-maintenance-mode' ); ?></p>
 			</div>
 
 			<div class="as-double-group as-clearfix">
 				<div class="as-form-group">
-					<label for="signals_csmm_input_size" class="as-strong"><?php esc_html_e( 'Input Text Size', 'classic-coming-soon-maintenance-mode' ); ?></label>
+					<label for="<?php echo Config::PREFIX . 'input_size'; ?>" class="as-strong"><?php esc_html_e( 'Input Text Size', 'classic-coming-soon-maintenance-mode' ); ?></label>
 
-					<select name="signals_csmm_input_size" id="signals_csmm_input_size">
+					<select name="<?php echo Config::PREFIX . 'input_size'; ?>" id="<?php echo Config::PREFIX . 'input_size'; ?>">
 						<?php
 
 							// Loading font sizes with the help of a loop
 							for ( $i = 11; $i < 41; $i++ ) {
-								echo '<option value="' . $i . '"' . selected( $options['input_font_size'], $i ) . '>' . $i . __( 'px', 'classic-coming-soon-maintenance-mode' ) . '</option>';
+								echo '<option value="' . $i . '"' . selected( $options['input_font_size'], $i ) . '>' . $i . esc_html__( 'px', 'classic-coming-soon-maintenance-mode' ) . '</option>';
 							}
 
 						?>
@@ -57,14 +57,14 @@ use AkshitSethi\Plugins\MaintenanceMode\Config;
 				</div>
 
 				<div class="as-form-group">
-					<label for="signals_csmm_button_size" class="as-strong"><?php esc_html_e( 'Button Text Size', 'classic-coming-soon-maintenance-mode' ); ?></label>
+					<label for="<?php echo Config::PREFIX . 'button_size'; ?>" class="as-strong"><?php esc_html_e( 'Button Text Size', 'classic-coming-soon-maintenance-mode' ); ?></label>
 
-					<select name="signals_csmm_button_size" id="signals_csmm_button_size">
+					<select name="<?php echo Config::PREFIX . 'button_size'; ?>" id="<?php echo Config::PREFIX . 'button_size'; ?>">
 						<?php
 
 							// Loading font sizes with the help of a loop
 							for ( $i = 11; $i < 41; $i++ ) {
-								echo '<option value="' . $i . '"' . selected( $options['button_font_size'], $i ) . '>' . $i . __( 'px', 'classic-coming-soon-maintenance-mode' ) . '</option>';
+								echo '<option value="' . $i . '"' . selected( $options['button_font_size'], $i ) . '>' . $i . esc_html__( 'px', 'classic-coming-soon-maintenance-mode' ) . '</option>';
 							}
 
 						?>
@@ -76,15 +76,15 @@ use AkshitSethi\Plugins\MaintenanceMode\Config;
 
 			<div class="as-double-group as-clearfix">
 				<div class="as-form-group">
-					<label for="signals_csmm_input_color" class="as-strong"><?php esc_html_e( 'Input Text Color', 'classic-coming-soon-maintenance-mode' ); ?></label>
-					<input type="text" name="signals_csmm_input_color" id="signals_csmm_input_color" value="<?php esc_attr_e( $options['input_font_color'] ); ?>" placeholder="<?php esc_html_e( 'Font color for the Input text', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control color {required:false}">
+					<label for="<?php echo Config::PREFIX . 'input_color'; ?>" class="as-strong"><?php esc_html_e( 'Input Text Color', 'classic-coming-soon-maintenance-mode' ); ?></label>
+					<input type="text" name="<?php echo Config::PREFIX . 'input_color'; ?>" id="<?php echo Config::PREFIX . 'input_color'; ?>" value="<?php esc_attr_e( $options['input_font_color'] ); ?>" placeholder="<?php esc_html_e( 'Font color for the Input text', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control color {required:false}">
 
 					<p class="as-form-help-block"><?php esc_html_e( 'Select font color for the input text field.', 'classic-coming-soon-maintenance-mode' ); ?></p>
 				</div>
 
 				<div class="as-form-group">
-					<label for="signals_csmm_button_color" class="as-strong"><?php esc_html_e( 'Button Text Color', 'classic-coming-soon-maintenance-mode' ); ?></label>
-					<input type="text" name="signals_csmm_button_color" id="signals_csmm_button_color" value="<?php esc_attr_e( $options['button_font_color'] ); ?>" placeholder="<?php esc_html_e( 'Font color for the Button text', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control color {required:false}">
+					<label for="<?php echo Config::PREFIX . 'button_color'; ?>" class="as-strong"><?php esc_html_e( 'Button Text Color', 'classic-coming-soon-maintenance-mode' ); ?></label>
+					<input type="text" name="<?php echo Config::PREFIX . 'button_color'; ?>" id="<?php echo Config::PREFIX . 'button_color'; ?>" value="<?php esc_attr_e( $options['button_font_color'] ); ?>" placeholder="<?php esc_html_e( 'Font color for the Button text', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control color {required:false}">
 
 					<p class="as-form-help-block"><?php esc_html_e( 'Select font color for the button text.', 'classic-coming-soon-maintenance-mode' ); ?></p>
 				</div>
@@ -92,15 +92,15 @@ use AkshitSethi\Plugins\MaintenanceMode\Config;
 
 			<div class="as-double-group as-clearfix">
 				<div class="as-form-group">
-					<label for="signals_csmm_input_bg" class="as-strong"><?php esc_html_e( 'Input Background Color', 'classic-coming-soon-maintenance-mode' ); ?></label>
-					<input type="text" name="signals_csmm_input_bg" id="signals_csmm_input_bg" value="<?php esc_attr_e( $options['input_bg'] ); ?>" placeholder="<?php esc_html_e( 'Background color for the Input field', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control color {required:false}">
+					<label for="<?php echo Config::PREFIX . 'input_bg'; ?>" class="as-strong"><?php esc_html_e( 'Input Background Color', 'classic-coming-soon-maintenance-mode' ); ?></label>
+					<input type="text" name="<?php echo Config::PREFIX . 'input_bg'; ?>" id="<?php echo Config::PREFIX . 'input_bg'; ?>" value="<?php esc_attr_e( $options['input_bg'] ); ?>" placeholder="<?php esc_html_e( 'Background color for the Input field', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control color {required:false}">
 
 					<p class="as-form-help-block"><?php esc_html_e( 'Select background color for the input text field.', 'classic-coming-soon-maintenance-mode' ); ?></p>
 				</div>
 
 				<div class="as-form-group">
-					<label for="signals_csmm_button_bg" class="as-strong"><?php esc_html_e( 'Button Background Color', 'classic-coming-soon-maintenance-mode' ); ?></label>
-					<input type="text" name="signals_csmm_button_bg" id="signals_csmm_button_bg" value="<?php esc_attr_e( $options['button_bg'] ); ?>" placeholder="<?php esc_html_e( 'Background color for the Button', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control color {required:false}">
+					<label for="<?php echo Config::PREFIX . 'button_bg'; ?>" class="as-strong"><?php esc_html_e( 'Button Background Color', 'classic-coming-soon-maintenance-mode' ); ?></label>
+					<input type="text" name="<?php echo Config::PREFIX . 'button_bg'; ?>" id="<?php echo Config::PREFIX . 'button_bg'; ?>" value="<?php esc_attr_e( $options['button_bg'] ); ?>" placeholder="<?php esc_html_e( 'Background color for the Button', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control color {required:false}">
 
 					<p class="as-form-help-block"><?php esc_html_e( 'Select background color for the button.', 'classic-coming-soon-maintenance-mode' ); ?></p>
 				</div>
@@ -108,15 +108,15 @@ use AkshitSethi\Plugins\MaintenanceMode\Config;
 
 			<div class="as-double-group as-clearfix">
 				<div class="as-form-group">
-					<label for="signals_csmm_input_bg_hover" class="as-strong"><?php esc_html_e( 'Input Background Color', 'classic-coming-soon-maintenance-mode' ); ?> <span class="as-red-color"><?php esc_html_e( 'FOCUS', 'classic-coming-soon-maintenance-mode' ); ?></span></label>
-					<input type="text" name="signals_csmm_input_bg_hover" id="signals_csmm_input_bg_hover" value="<?php esc_attr_e( $options['input_bg_hover'] ); ?>" placeholder="<?php esc_html_e( 'Background color for the Input field when it gets clicked', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control color {required:false}">
+					<label for="<?php echo Config::PREFIX . 'input_bg_hover'; ?>" class="as-strong"><?php esc_html_e( 'Input Background Color', 'classic-coming-soon-maintenance-mode' ); ?> <span class="as-red-color"><?php esc_html_e( 'FOCUS', 'classic-coming-soon-maintenance-mode' ); ?></span></label>
+					<input type="text" name="<?php echo Config::PREFIX . 'input_bg_hover'; ?>" id="<?php echo Config::PREFIX . 'input_bg_hover'; ?>" value="<?php esc_attr_e( $options['input_bg_hover'] ); ?>" placeholder="<?php esc_html_e( 'Background color for the Input field when it gets clicked', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control color {required:false}">
 
 					<p class="as-form-help-block"><?php esc_html_e( 'Select background color for the input text field when it gets clicked.', 'classic-coming-soon-maintenance-mode' ); ?></p>
 				</div>
 
 				<div class="as-form-group">
-					<label for="signals_csmm_button_bg_hover" class="as-strong"><?php esc_html_e( 'Button Background Color', 'classic-coming-soon-maintenance-mode' ); ?> <span class="as-red-color"><?php esc_html_e( 'HOVER', 'classic-coming-soon-maintenance-mode' ); ?></span></label>
-					<input type="text" name="signals_csmm_button_bg_hover" id="signals_csmm_button_bg_hover" value="<?php esc_attr_e( $options['button_bg_hover'] ); ?>" placeholder="<?php esc_html_e( 'Background color for the Button on hover', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control color {required:false}">
+					<label for="<?php echo Config::PREFIX . 'button_bg_hover'; ?>" class="as-strong"><?php esc_html_e( 'Button Background Color', 'classic-coming-soon-maintenance-mode' ); ?> <span class="as-red-color"><?php esc_html_e( 'HOVER', 'classic-coming-soon-maintenance-mode' ); ?></span></label>
+					<input type="text" name="<?php echo Config::PREFIX . 'button_bg_hover'; ?>" id="<?php echo Config::PREFIX . 'button_bg_hover'; ?>" value="<?php esc_attr_e( $options['button_bg_hover'] ); ?>" placeholder="<?php esc_html_e( 'Background color for the Button on hover', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control color {required:false}">
 
 					<p class="as-form-help-block"><?php esc_html_e( 'Select background color for the button on mouse hover.', 'classic-coming-soon-maintenance-mode' ); ?></p>
 				</div>
@@ -124,15 +124,15 @@ use AkshitSethi\Plugins\MaintenanceMode\Config;
 
 			<div class="as-double-group as-clearfix">
 				<div class="as-form-group">
-					<label for="signals_csmm_input_border" class="as-strong"><?php esc_html_e( 'Input Border Color', 'classic-coming-soon-maintenance-mode' ); ?></label>
-					<input type="text" name="signals_csmm_input_border" id="signals_csmm_input_border" value="<?php esc_attr_e( $options['input_border'] ); ?>" placeholder="<?php esc_html_e( 'Border color for the Input field', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control color {required:false}">
+					<label for="<?php echo Config::PREFIX . 'input_border'; ?>" class="as-strong"><?php esc_html_e( 'Input Border Color', 'classic-coming-soon-maintenance-mode' ); ?></label>
+					<input type="text" name="<?php echo Config::PREFIX . 'input_border'; ?>" id="<?php echo Config::PREFIX . 'input_border'; ?>" value="<?php esc_attr_e( $options['input_border'] ); ?>" placeholder="<?php esc_html_e( 'Border color for the Input field', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control color {required:false}">
 
 					<p class="as-form-help-block"><?php esc_html_e( 'Select border color for the input field.', 'classic-coming-soon-maintenance-mode' ); ?></p>
 				</div>
 
 				<div class="as-form-group">
-					<label for="signals_csmm_button_border" class="as-strong"><?php esc_html_e( 'Button Border Color', 'classic-coming-soon-maintenance-mode' ); ?></label>
-					<input type="text" name="signals_csmm_button_border" id="signals_csmm_button_border" value="<?php esc_attr_e( $options['button_border'] ); ?>" placeholder="<?php esc_html_e( 'Border color for the Button', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control color {required:false}">
+					<label for="<?php echo Config::PREFIX . 'button_border'; ?>" class="as-strong"><?php esc_html_e( 'Button Border Color', 'classic-coming-soon-maintenance-mode' ); ?></label>
+					<input type="text" name="<?php echo Config::PREFIX . 'button_border'; ?>" id="<?php echo Config::PREFIX . 'button_border'; ?>" value="<?php esc_attr_e( $options['button_border'] ); ?>" placeholder="<?php esc_html_e( 'Border color for the Button', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control color {required:false}">
 
 					<p class="as-form-help-block"><?php esc_html_e( 'Select border color for the button.', 'classic-coming-soon-maintenance-mode' ); ?></p>
 				</div>
@@ -140,15 +140,15 @@ use AkshitSethi\Plugins\MaintenanceMode\Config;
 
 			<div class="as-double-group as-clearfix">
 				<div class="as-form-group">
-					<label for="signals_csmm_input_border_hover" class="as-strong"><?php esc_html_e( 'Input Border Color', 'classic-coming-soon-maintenance-mode' ); ?> <span class="as-red-color"><?php esc_html_e( 'FOCUS', 'classic-coming-soon-maintenance-mode' ); ?></span></label>
-					<input type="text" name="signals_csmm_input_border_hover" id="signals_csmm_input_border_hover" value="<?php esc_attr_e( $options['input_border_hover'] ); ?>" placeholder="<?php esc_html_e( 'Border color for the Input field when it gets clicked', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control color {required:false}">
+					<label for="<?php echo Config::PREFIX . 'input_border_hover'; ?>" class="as-strong"><?php esc_html_e( 'Input Border Color', 'classic-coming-soon-maintenance-mode' ); ?> <span class="as-red-color"><?php esc_html_e( 'FOCUS', 'classic-coming-soon-maintenance-mode' ); ?></span></label>
+					<input type="text" name="<?php echo Config::PREFIX . 'input_border_hover'; ?>" id="<?php echo Config::PREFIX . 'input_border_hover'; ?>" value="<?php esc_attr_e( $options['input_border_hover'] ); ?>" placeholder="<?php esc_html_e( 'Border color for the Input field when it gets clicked', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control color {required:false}">
 
 					<p class="as-form-help-block"><?php esc_html_e( 'Select border color for the input field when it gets clicked.', 'classic-coming-soon-maintenance-mode' ); ?></p>
 				</div>
 
 				<div class="as-form-group">
-					<label for="signals_csmm_button_border_hover" class="as-strong"><?php esc_html_e( 'Button Border Color', 'classic-coming-soon-maintenance-mode' ); ?> <span class="as-red-color"><?php esc_html_e( 'HOVER', 'classic-coming-soon-maintenance-mode' ); ?></span></label>
-					<input type="text" name="signals_csmm_button_border_hover" id="signals_csmm_button_border_hover" value="<?php esc_attr_e( $options['button_border_hover'] ); ?>" placeholder="<?php esc_html_e( 'Border color for the Button on hover', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control color {required:false}">
+					<label for="<?php echo Config::PREFIX . 'button_border_hover'; ?>" class="as-strong"><?php esc_html_e( 'Button Border Color', 'classic-coming-soon-maintenance-mode' ); ?> <span class="as-red-color"><?php esc_html_e( 'HOVER', 'classic-coming-soon-maintenance-mode' ); ?></span></label>
+					<input type="text" name="<?php echo Config::PREFIX . 'button_border_hover'; ?>" id="<?php echo Config::PREFIX . 'button_border_hover'; ?>" value="<?php esc_attr_e( $options['button_border_hover'] ); ?>" placeholder="<?php esc_html_e( 'Border color for the Button on hover', 'classic-coming-soon-maintenance-mode' ); ?>" class="as-form-control color {required:false}">
 
 					<p class="as-form-help-block"><?php esc_html_e( 'Select border color for the button on mouse hover.', 'classic-coming-soon-maintenance-mode' ); ?></p>
 				</div>
