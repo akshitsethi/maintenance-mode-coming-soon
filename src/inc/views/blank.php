1c8727ef8dc83fac0d88a86051d47f59b4ff65c7
Akshit Sethi <akshitsethi@gmail.com>
2020-03-31 21:35:17 +0530

    Refactored the frontend templates

--- src/inc/views/blank.php
+++ src/inc/views/blank.php
@@ -1,11 +1,8 @@
 <?php
-
 /**
- * Renders the blank template for the plugin.
+ * Renders the blank template for the frontend.
  *
- * @link       http://www.69signals.com
- * @since      1.0
- * @package    Signals_Maintenance_Mode
+ * @since 1.0.0
  */
 
 ?><!DOCTYPE html>
@@ -14,109 +11,128 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title><?php echo $options['title']; ?></title>
-<?php if ( ! empty( $options['favicon'] ) ) : ?>
-<link rel="shortcut icon" href="<?php echo esc_url_raw( $options['favicon'] ); ?>" />
+<title><?php echo stripslashes( $this->get_option( sanitize_text_field( $options['title'] ), esc_html__( 'Maintainance Mode', 'classic-coming-soon-maintenance-mode' ) ) ); ?></title>
+<?php if ( isset( $options['favicon'] ) && ! empty( $options['favicon'] ) ) : ?>
+<link rel="shortcut icon" href="<?php echo esc_url_raw( sanitize_text_field( $options['favicon'] ) ); ?>" />
 <?php endif; ?>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 <link rel="alternate" type="application/atom+xml" title="<?php bloginfo('name'); ?> Atom Feed" href="<?php bloginfo('atom_url'); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<link rel="stylesheet" type="text/css" href="<?php echo SIGNALS_CSMM_URL; ?>/framework/public/css/basic.css" />
-<script src='//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js'></script>
-<script>
-	WebFont.load( {
+<link rel="stylesheet" type="text/css" href="<?php echo Config::$plugin_url; ?>assets/css/front.css">
+<script src="<?php echo Config::$plugin_url; ?>assets/admin/js/webfont.js"></script>
+<script type="text/javascript">
+	var data = {
+		header_font: <?php echo $options['header_font']; ?>,
+		secondary_font: <?php echo $options['secondary_font']; ?>
+	}
+
+	// Font loader
+	WebFont.load({
 		google: {
-			families: ['<?php echo $options["header_font"]; ?>', '<?php echo $options["secondary_font"]; ?>']
+			families: [data.header_font, data.secondary_font]
 		}
-	} );
+	});
 </script>
 <?php
 
-	// user defined css for the blank mode
+	// Custom CSS for the blank template
 	if ( ! empty( $options['custom_css'] ) ) {
 		echo '<style>';
-		echo stripslashes( $options['custom_css'] );
+		echo stripslashes( strip_tags( $options['custom_css'] ) );
 		echo '</style>';
 	}
 
 ?>
 </head>
 <body>
-	<?php
+<?php
 
-		// Custom html
-		// Nothing else will be included here since we are serving a blank template
-		$custom_html = stripslashes( $options['custom_html'] );
+	// Custom HTML
+	// Nothing else will be included here since we are serving a blank template
+	$custom_html = stripslashes( $options['custom_html'] );
 
-		// form
-		if ( ! empty( $custom_html ) && false !== strpos( $custom_html, '{{form}}' ) ) {
-			if ( ! empty( $options['mailchimp_api'] ) && ! empty( $options['mailchimp_list'] ) ) {
-				// Checking if the form is submitted or not
-				if ( isset( $_POST['signals_email'] ) ) {
-					// Processing begins
-					$signals_email = strip_tags( $_POST['signals_email'] );
+	// Form
+	if ( ! empty( $custom_html ) && false !== strpos( $custom_html, '{{form}}' ) ) {
+		// Form
+		if ( ! empty( $options['mailchimp_api'] ) && ! empty( $options['mailchimp_list'] ) ) {
+			// Checking if the form is submitted or not
+			if ( isset( $_POST[Config::PREFIX . 'email'] ) ) {
+				$email = strip_tags( $_POST[Config::PREFIX . 'email'] );
 
-					if ( '' === $signals_email ) {
-						$code 		= 'danger';
-						$response 	= __( 'Please provide your email address.', 'signals' );
+				if ( empty( $email ) ) {
+					$code 		= 'error';
+					$response = $this->get_option( sanitize_text_field( $options['message_noemail'] ), esc_html__( 'Please provide your email address.', 'classic-coming-soon-maintenance-mode' ) );
+				} else {
+					$email = filter_var( strtolower( trim( $email ) ), FILTER_SANITIZE_EMAIL );
+
+					// Check value for filter_var
+					if ( ! $email ) {
+						$code 		= 'error';
+						$response = $this->get_option( sanitize_text_field( $options['message_wrong'] ), esc_html__( 'Please provide a valid email address.', 'classic-coming-soon-maintenance-mode' ) );
 					} else {
-						$signals_email = filter_var( strtolower( trim( $signals_email ) ), FILTER_SANITIZE_EMAIL );
+						$mailchimp = new MailChimp( sanitize_text_field( $options['mailchimp_api'] ) );
+						$connect   = $mailchimp->post(
+							'lists/' . sanitize_text_field( $options['mailchimp_list'] ) . '/members',
+							array(
+								'email_address' => $email,
+								'status'        => 'subscribed',
+							)
+						);
 
-						if ( strpos( $signals_email, '@' ) ) {
-							require_once SIGNALS_CSMM_PATH . '/framework/admin/include/classes/class-mailchimp.php';
+						// Show the response
+						if ( $mailchimp->success() ) {
+							$code 		= 'success';
 
-							$signals_connect 	= new Signals_MailChimp( $options['mailchimp_api'] );
-							$signals_response 	= $signals_connect->call( 'lists/subscribe', array(
-								'id'            => $options['mailchimp_list'],
-								'email'         => array( 'email' => $signals_email ),
-								'send_welcome'  => true
-							) );
-
-
-							// Showing message as per the response from the mailchimp server
-							if ( isset( $signals_response['code'] ) && 214 !== $signals_response['code'] ) {
-								$code 		= 'danger';
-								$response 	= $options['message_wrong'];
-							} elseif ( isset( $signals_response['code'] ) && 214 === $signals_response['code'] ) {
-								$code 		= 'success';
-								$response 	= $options['message_subscribed'];
-							} else {
-								$code 		= 'success';
-								$response 	= $options['message_done'];
-							}
+							// Show the success message
+							$response = $this->get_option( sanitize_text_field( $options['message_done'] ), esc_html__( 'Thank you! We\'ll be in touch!', 'classic-coming-soon-maintenance-mode' ) );
 						} else {
-							$code 			= 'danger';
-							$response 		= $options['message_noemail'];
+							$response['code'] = 'error';
+							$response['text'] = $mailchimp->getLastError();
 						}
 					}
-				} // signals_email
-
-				// Subscription form
-				// Displaying errors as well if they are set
-				$subscription_form = '<div class="subscription">';
-
-				if ( isset( $code ) && isset( $response ) ) {
-					$subscription_form .= '<div class="signals-alert signals-alert-' . $code . '">' . $response . '</div>';
 				}
+			} else {
+				$code 		= 'error';
+				$response = $this->get_option( sanitize_text_field( $options['message_error'] ), esc_html__( 'This looks like an invalid request. Please try again.', 'classic-coming-soon-maintenance-mode' ) );
+			}
 
-				$subscription_form .= '<form role="form" method="post">
-					<input type="text" name="signals_email" placeholder="' . esc_attr( $options['input_text'] ) . '">
-					<input type="submit" name="submit" value="' . esc_attr( $options['button_text'] ) . '">
-				</form>';
-				$subscription_form .= '</div>';
+			// Subscription form
+			// Displaying errors as well if they are set
+			$subcription_form = '<div class="as-subscription">';
 
-				// Replacing the form placeholder
-				$custom_html = str_replace( '{{form}}', $subscription_form, $custom_html );
-			} // mailchimp_api && mailchimp_list
-		} // custom_html
+			if ( isset( $code ) && isset( $response ) ) {
+				$subcription_form .= '<div class="as-alert as-alert-' . $code . '">' . $response . '</div>';
+			}
 
-		// Output the user defined html
-		echo $custom_html;
+			$subcription_form .= '<form role="form" method="post">
+				<input type="text" name="' . Config::PREFIX . 'email" placeholder="' . esc_attr( $this->get_option( sanitize_text_field( $options['input_text'] ), esc_html__( 'Enter your email address..', 'classic-coming-soon-maintenance-mode' ) ) ) . '">
+				<input type="' . Config::PREFIX . 'submit" name="' . Config::PREFIX . 'submit" value="' . esc_attr( $this->get_option( sanitize_text_field( $options['button_text'] ), esc_html__( 'Subscribe', 'classic-coming-soon-maintenance-mode' ) ) ) . '">
+			</form>';
 
-	?>
+			// Antispam text
+			if ( ! empty( $options['antispam_text'] ) ) {
+				$subcription_form .= '<p class="anti-spam">' . stripslashes( sanitize_text_field( $options['antispam_text'] ) ) . '</p>';
+			}
 
-<!-- Maintenance Mode Plugin by 69signals (http://www.69signals.com) -->
-<!-- We are a creative digital marketplace. We love to weave the web, simple but amazing. We create flawless web and mobile applications. -->
+			$subcription_form .= '</div><!-- .as-subscription -->';
+
+			// Replace {{form}} placeholder
+			$custom_html = str_replace( '{{form}}', $subscription_form, $custom_html );
+		}
+	}
+
+	// Output HTML
+	echo $custom_html;
+
+	// Analytics code
+	if ( isset( $options['analytics'] ) && ! empty( $options['analytics'] ) ) {
+		echo stripslashes( $options['analytics'] ) . "\r\n";
+	}
+
+?>
+
+<!-- Classic Coming Soon & Maintenance Mode Plugin by Akshit Sethi (https://akshitsethi.com) -->
+<!-- Twitter: @akshitsethi -->
 </body>
 </html>
