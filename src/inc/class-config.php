babf2dffcb2ad9d3f9874e508270bbd0cb67b280
Akshit Sethi <akshitsethi@gmail.com>
2020-04-08 07:59:34 +0530

    Bump plugin version and update the readme file

--- src/inc/class-config.php
+++ src/inc/class-config.php
@@ -18,7 +18,7 @@ class Config {
 
 	const PLUGIN_SLUG   = 'maintenance-mode-coming-soon';
 	const SHORT_SLUG    = 'mmcs';
-	const VERSION       = '1.0.1';
+	const VERSION       = '1.5.0';
 	const DB_OPTION     = 'as_' . self::SHORT_SLUG;
 	const PREFIX        = self::SHORT_SLUG . '_';
 	const DEFAULT_FONTS = array(
